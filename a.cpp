c038be6b76f1b6873c284670a7f325ef8944f8fc
Rajat Jain <rajat.rj10@gmail.com>
2015-01-04 15:35:33 +0530

    added new files

--- a.cpp
+++ a.cpp
@@ -1,9 +1,9 @@
 #include <iostream>
 using namespace std;
 int main() {
-	int x;
-	cin >> x;
-	for(int i = 1; i <= 10; i++) {
-		cout << i << endl;	
-	}
+
+  for(int i = 1; i <= 100; i++) {
+    cout << i << endl;
+  }
+
 }
